9931b3b092bf0626b24162904cd967a2f6e065e0
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-10-07 23:04:12 +0700

    Menambahkan Auto Loading

---
+++ oophp/Auto Loading/App/Produk/Coba.php
@@ -0,0 +1,8 @@
+<?php
+
+class Coba {
+    public function __construct()
+    {
+        echo "Ini adalah kelas Coba";
+    }
+}
---
+++ oophp/Auto Loading/App/init.php
@@ -0,0 +1,10 @@
+<?php
+// require_once 'App/Produk/InfoProduk.php';
+// require_once 'App/Produk/Produk.php';
+// require_once 'App/Produk/Komik.php';
+// require_once 'App/Produk/Game.php';
+// require_once 'App/Produk/CetakInfoProduk.php';
+
+spl_autoload_register(function($class){
+    require_once __DIR__ . '/Produk/' . $class . '.php';
+});
