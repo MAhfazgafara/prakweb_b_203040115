55a6b79832a3fa1ee8b67bdbbf4b1ecd0704fac0
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-10-13 20:50:29 +0700

    Routing

--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -3,6 +3,17 @@
 class App {
     public function __construct()
     {
-        echo 'OK!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parse()
+    {
+        if( isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
