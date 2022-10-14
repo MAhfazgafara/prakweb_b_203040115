f32b75c56859d4c92804e2eb019761f8b7cec09c
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-10-14 20:37:56 +0700

    View

--- phpmvc/app/controllers/Home.php
+++ phpmvc/app/controllers/Home.php
@@ -1,8 +1,13 @@
 <?php
 
-class Home {
+class Home extends Controller
+{
     public function index()
     {
-        echo 'home/index';
+        $data['judul'] = 'Home';
+        $data['nama'] = $this->model('UserModel')->getUser();
+        $this->view('templates/header', $data);
+        $this->view('home/index', $data);
+        $this->view('templates/footer',);
     }
 }
---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <h1 class='mt-4'>About Me</h1>
+    <img src="<?= BASEURL ?>/img/" alt="Ahfaz Gafara" width="200" class="rounded-circle shadow">
+    <p>Hola, Nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> Tahun, Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+    <a class="btn btn-info btn-lg" href="<?= BASEURL ?>/home" role="button">Home</a>
+
+</div>
