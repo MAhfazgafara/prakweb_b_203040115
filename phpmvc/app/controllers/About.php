1ec16ef93e6dca0fbdc7214df40e3eb14b749f66
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-10-14 19:11:06 +0700

    Controller

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About {
-    public function index($nama = 'Ahfaz', $pekerjaan = 'Mahasiswa')
+class About extends Controller
+{
+    public function index($nama = 'Ahfaz Gafara', $pekerjaan = 'Mahasiswa', $umur = 20)
     {
-        echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
-
-    public function page() 
+    public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
