b38eb82bcbbe878815e4cc45d1ca3997226dd06a
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-10-14 19:13:15 +0700

    core

--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -1,48 +1,50 @@
 <?php
 
-class App {
-    protected $controller = 'Home';
-    protected $method = 'index';
-    protected $params = [];
+class App
+{
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
 
-    public function __construct()
-    {
-        $url = $this->parseURL();
-        
-        if( file_exists('../app/controllers/' . $url[0] . '.php')  ) {
-            $this->controller = $url[0];
-            unset($url[0]);
+  public function __construct()
+  {
+    $url = $this->parseURL();
+    if ($url == NULL) {
+      $url = [$this->controller];
+    }
+        // CONTROLLER
+        if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+          $this->controller = $url[0];
+          unset($url[0]);
         }
-
+    
         require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
-
-
-        // method
-        if( isset($url[1]) ) {
-            if( method_exists($this->controller, $url[1] ) {
-                $this->method = $url[1];
-                unset($url[1]);
-            }
+    
+        // METHOD
+        if (isset($url[1])) {
+          if (method_exists($this->controller, $url[1])) {
+            $this->method = $url[1];
+            unset($url[1]);
+          }
         }
-
-        // params
-        if ( !empty($url) ) {
-            $this->params = array_values($url);
+    
+        // PARAMS
+        if (!empty($url)) {
+          $this->params = array_values($url);
         }
+    
+        // JALANKAN CONTROLLER & METHOD, SERTA KIRIM PARAMS JIKA ADA
+        call_user_func_array([$this->controller, $this->method], $this->params);
+  }
 
-        // jalankan controller & method, serta kirimkan pramas jika ada
-        call_user_func_array([$this_>controller, $this->method], $this->params);
-
+  public function parseURL()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL,);
+      $url = explode('/', $url);
+      return $url;
     }
-
-    public function parse()
-    {
-        if( isset($_GET['url'])) {
-            $url = rtrim($_GET['url'], '/');
-            $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode('/', $url);
-            return $url;
-        }
-    }
+  }
 }
